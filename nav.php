36c6dd17fbdcfccdc9459ca8d93d368171f5f8c8
Dillon Ellis <dellis@fcul.com>
2022-08-22 09:38:46 -0400

    original nav

---
+++ nav.php
@@ -0,0 +1,89 @@
+<div class="container">
+          <div class="mod-menu">
+            <div class="row">
+              <div class="col-sm-2"> <a href="/index.php" class="logo style-2"> <img src="/images/logo.png" alt="League of Southeastern Credit Unions & Affiliates"> </a> </div>
+              <div class="col-sm-10">
+                <div class="main-nav">
+                  <ul class="nav navbar-nav top-nav">
+                    <li class="search-parent"> <a href="/pro-search/search.php" aria-label="Search Icon"><i aria-hidden="true" class="fa fa-search"></i></a></li>
+
+                    <li class="visible-xs menu-icon"> <a href="javascript:void(0)" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu" aria-expanded="false" aria-label="Mobile Menu Icon"> <i aria-hidden="true" class="fa fa-bars"></i> </a> </li>
+                  </ul>
+                  <div id="menu" class="collapse">
+                    <ul class="nav navbar-nav">
+                      <li class="right"> <a href="/advocacy/legislative/index.php">ADVOCACY</a> <span class="arrow"></span>
+                        <ul class="dm-align-2">
+                          <li><a href="/advocacy/legislative/index.php">Legislative Advocacy</a></li>
+                <li><a href="/advocacy/grassroots.php">Grassroots Advocacy</a></li>
+							
+                <li><a href="/advocacy/pacs.php">Political Action Committees</a></li>
+				<li><a href="/advocacy/regulatory.php">Regulatory Advocacy</a></li>
+				<li><a href="https://lscu.coop/creditunionsvote/" target="_blank">Get Out The Vote</a></li>
+				<!--<li><a href="/publications/ci/index.php">State Advocacy Publications</a></li>-->
+                        </ul>
+                      </li>
+                      <li> <a href="/compliance/index.php">COMPLIANCE</a> <span class="arrow"></span>
+                        <ul class="dm-align-2">
+                          <li><a href="/compliance/comms/index.php">LSCU Compliance Resources</a></li>
+                <li><a href="/compliance/resources/index.php">State & Federal Compliance Resources</a></li>
+				  <li><a href="/advocacy/regulatory.php">Regulatory Advocacy</a></li>
+				  <li><a href="/compliance/programs.php">Shared Compliance Consultant Program</a></li>
+				  <li><a href="/compliance/research/index.php">Research & Economic Data</a></li>
+                        </ul>
+                      </li>
+                      <li> <a href="/education/index.php">EDUCATION</a> <span class="arrow"></span>
+                        <ul class="dm-align-2">
+							<li><a href="/about/opportunities.php">Advertising/Sponsorship Opportunities</a></li>
+                          
+							<li><a href="/temp/2022-LSCU-Events.pdf" target="_blank">2022 Education & Training Calendar (PDF)</a></li>
+                <li><a href="/education/presenters.php">Call for Presenters</a></li>
+                <li><a href="/education/conferences.php">Conferences & Signature Events</a></li>
+				<li><a href="/education/training.php">Schools, Training & Workshops</a></li>
+                <li><a href="/education/education.php">Continuing Professional Education</a></li>
+				<li><a href="http://lscucouncils.com" target="_blank">Credit Union Networking Councils</a></li>
+				<li><a href="/foundation/scholarships.php">Professional Development Grants</a></li>
+                        </ul>
+                      </li>
+                      <li> <a href="/member-engagement/index.php">MEMBER ENGAGEMENT</a> <span class="arrow"></span>
+                        <ul class="dm-align-2">
+                          <li><a href="/about/membership.php">LSCU Membership</a></li>
+							<li><a href="/member-engagement/league.php">League 101</a></li>
+							<li><a href="https://wp.lscu.coop/calendar">Calendar</a></li>
+                <li><a href="/member-engagement/chapters/index.php">Chapters</a></li>
+                <li><a href="/member-engagement/international.php">International Partnerships</a></li>
+                <li><a href="/member-engagement/small-cu/index.php">Small Credit Union Program</a></li>
+				<li><a href="/member-engagement/yp.php">Young Professionals Group</a></li>
+                        </ul>
+                      </li>
+                      <li> <a href="/comms/index.php">COMMUNICATIONS</a> <span class="arrow"></span>
+                        <ul class="dm-align-2">
+                          <li><a href="/comms/awards.php">Awards Programs</a></li>
+							<li><a href="/comms/media.php">Media Resources</a></li>
+							<li><a href="/oye/index.php">Open Your Eyes</a></li>
+						  <li><a href="/comms/press-releases.php">Press Releases</a></li>
+							<li><a href="/comms/publications.php">Publications</a></li>
+							
+						  <li><a href="/comms/social.php">Social Media Channels</a></li>
+                        </ul>
+                      </li>
+                      <li> <a href="/foundation/index.php">FOUNDATION</a> <span class="arrow"></span>
+                        <ul class="dm-align-2">
+							<li><a href="/foundation/cu-emp-financial-wellness.php">CU Employees Financial Wellness</a></li>
+                          <li><a href="/foundation/cus-care.php">Credit Unions Care Challenge</a></li>
+							<li><a href="/foundation/financial-programs.php">Financial Capability Programs</a></li>
+				  <li><a href="/foundation/community.php">Community Development</a></li>
+				  <li><a href="/foundation/ficep.php">FiCEP Certification</a></li>
+                <li><a href="/foundation/scholarships.php">Professional Development Grants</a></li>
+                <li><a href="/foundation/disaster.php">Disaster Preparedness & Relief</a></li>
+                <li><a href="/foundation/support.php">Support the Foundation</a></li>
+                        </ul>
+                      </li>
+					  <li class="right"> <a href="http://myleverage.com/" target="_blank"><em>LEVERAGE</em></a> <span class="arrow"></span></li>
+                      
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
