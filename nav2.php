fa51430e1387698eded6879296f70c2a572d0e1d
Dillon Ellis <dellis@fcul.com>
2022-08-22 09:39:57 -0400

    original nav2 and top bar

---
+++ nav2.php
@@ -0,0 +1,87 @@
+<div class="container">
+          <div class="mod-menu">
+            <div class="row">
+              <div class="col-sm-2"> <a href="https://www.lscu.coop/index.php" class="logo style-2"> <img src="images/logo.png" alt=""> </a> </div>
+              <div class="col-sm-10">
+                <div class="main-nav">
+                  <ul class="nav navbar-nav top-nav">
+                    <li class="search-parent"> <a href="javascript:void(0)" title=""><i aria-hidden="true" class="fa fa-search"></i></a>
+                      <div class="search-box ">
+                        <div class="content">
+                          <div class="form-control">
+                            <input type="text" placeholder="Type to search" />
+                            <a href="#" class="search-btn mar-1"><i aria-hidden="true" class="fa fa-search"></i></a> </div>
+                          <a href="#" class="close-btn mar-1">x</a> </div>
+                      </div>
+                    </li>
+
+                    <li class="visible-xs menu-icon"> <a href="javascript:void(0)" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu" aria-expanded="false"> <i aria-hidden="true" class="fa fa-bars"></i> </a> </li>
+                  </ul>
+                  <div id="menu" class="collapse">
+                    <ul class="nav navbar-nav">
+                      <li class="right"> <a href="https://www.lscu.coop/advocacy/legislative.php">ADVOCACY</a> <span class="arrow"></span>
+                        <ul class="dm-align-2">
+                          <li><a href="https://www.lscu.coop/advocacy/legislative.php">Legislative Advocacy</a></li>
+                <li><a href="https://www.lscu.coop/advocacy/grassroots.php">Grassroots Advocacy</a></li>
+                <li><a href="https://www.lscu.coop/advocacy/pacs.php">Political Action Committees</a></li>
+				<li><a href="https://www.lscu.coop/advocacy/regulatory.php">Regulatory Advocacy</a></li>
+				<li><a href="https://www.lscu.coop/publications/ci/index.php">State Advocacy Publications</a></li>
+                        </ul>
+                      </li>
+                      <li> <a href="https://www.lscu.coop/compliance/index.php">COMPLIANCE</a> <span class="arrow"></span>
+                        <ul class="dm-align-2">
+                          <li><a href="https://www.lscu.coop/compliance/resources.php">Compliance Resource Links</a></li>
+                <li><a href="https://www.lscu.coop/compliance/communications.php">Compliance Communications</a></li>
+				  <li><a href="https://www.lscu.coop/compliance/docs/LSCU-Compliance-Calendar.pdf" target="_blank">Compliance Calendar</a></li>
+				  <li><a href="https://www.lscu.coop/compliance/programs.php">Compliance Programs</a></li>
+				  <li><a href="https://www.lscu.coop/compliance/research.php">Research & Economic Data</a></li>
+                        </ul>
+                      </li>
+                      <li> <a href="https://www.lscu.coop/education/index.php">EDUCATION</a> <span class="arrow"></span>
+                        <ul class="dm-align-2">
+							<li><a href="https://www.lscu.coop/about/opportunities.php">Advertising/Sponsorship Opportunities</a></li>
+                          <li><a href="https://wp.lscu.coop/calendar">Calendar</a></li>
+                <li><a href="https://www.lscu.coop/education/presenters.php">Call for Presenters</a></li>
+                <li><a href="https://www.lscu.coop/education/conferences.php">Conferences, Schools & Workshops</a></li>
+                <li><a href="https://www.lscu.coop/education/education.php">Continuing Professional Education</a></li>
+                <li><a href="https://www.lscu.coop/education/cancellation-policies.php">Event Cancellation Policies</a></li>
+				<li><a href="https://www.lscu.coop/education/scholarships.php">Scholarships</a></li>
+                        </ul>
+                      </li>
+                      <li> <a href="https://www.lscu.coop/member-engagement/index.php">MEMBER ENGAGEMENT</a> <span class="arrow"></span>
+                        <ul class="dm-align-2">
+                          <li><a href="https://www.lscu.coop/about/membership.php">LSCU Membership</a></li>
+                <li><a href="https://www.lscu.coop/member-engagement/chapters.php">Chapters</a></li>
+                <li><a href="https://www.lscu.coop/member-engagement/councils.php">Credit Union Networking Councils</a></li>
+                <li><a href="https://www.lscu.coop/member-engagement/international.php">International Partnerships</a></li>
+                <li><a href="https://www.lscu.coop/member-engagement/smallcu.php">Small Asset Size CU Program</a></li>
+				<li><a href="https://www.lscu.coop/member-engagement/yp.php">Young Professionals Group</a></li>
+                        </ul>
+                      </li>
+                      <li> <a href="https://www.lscu.coop/comms/index.php">COMMUNICATION</a> <span class="arrow"></span>
+                        <ul class="dm-align-2">
+                          <li><a href="https://www.lscu.coop/comms/press-room.php">Press Room</a></li>
+                <li><a href="https://www.lscu.coop/comms/publications.php">Publications</a></li>
+                <li><a href="https://www.lscu.coop/comms/awards.php">Awards Programs</a></li>
+							<li><a href="https://www.lscu.coop/comms/social.php">Social Media Channels</a></li>
+                        </ul>
+                      </li>
+                      <li> <a href="https://www.lscu.coop/foundation/index.php">FOUNDATION</a> <span class="arrow"></span>
+                        <ul class="dm-align-2">
+                          <li><a href="https://www.lscu.coop/foundation/financial-programs.php">Financial Education Programs</a></li>
+				  <li><a href="https://www.lscu.coop/foundation/community.php">Community Development</a></li>
+				  <li><a href="https://www.lscu.coop/foundation/ficep.php">FiCEP Certification</a></li>
+                <li><a href="https://www.lscu.coop/foundation/scholarships.php">Professional Development Scholarships</a></li>
+                <li><a href="https://www.lscu.coop/foundation/disaster.php">Disaster Preparedness & Relief</a></li>
+                <li><a href="https://www.lscu.coop/foundation/support.php">Support the Foundation</a></li>
+                        </ul>
+                      </li>
+					  <li class="right"> <a href="http://myleverage.com/"><em>LEVERAGE</em></a> <span class="arrow"></span></li>
+                      
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
---
+++ topbar.php
@@ -0,0 +1,21 @@
+<div class="container"> 
+    <div class="topbar-left-items">
+    <ul class="toplist toppadding pull-left paddtop1">
+		<li class="rightl lineright"><a href="https://pardot.lscu.coop/l/513811/2020-08-03/3s98qdw">Sign Up For News</a></li>
+		<li class="lineright"><a href="https://lscu.coop/about/contact.php">Contact Us</a></li>
+		<li class="lineright"><a href="https://lscu.coop/about/jobs.php">Job Center</a></li>
+		<li class="lineright"><a href="https://lscu.coop/about/index.php">About Us</a></li>
+		<li><a href="https://lscu.coop/about/welcome/index.php">LSCU LOGIN</a></li>
+      </ul>
+    </div> 
+    <!--end left-->
+    
+    <div class="topbar-right-items pull-right">
+    <ul class="toplist toppadding">
+        <li><a href="https://www.facebook.com/LeagueofSoutheasternCreditUnions?ref=hl" target="_blank" aria-label="Facebook Icon"><i class="fa fa-facebook"><span style="display: none;">Facebook Icon</span></i></a></li>
+        <li><a href="https://twitter.com/LeagueofSECUs" target="_blank" aria-label="Twitter Icon"><i class="fa fa-twitter"><span style="display: none;">Twitter Icon</span></i></a></li>
+        <li><a href="https://www.youtube.com/user/LeagueofSECUs" target="_blank" aria-label="YouTube Icon"><i class="fa fa-youtube"><span style="display: none;">YouTube Icon</span></i></a></li>
+        <li class="last"><a href="https://www.linkedin.com/company/779108?trk=tyah&trkInfo=clickedVertical%3Acompany%2Cidx%3A1-1-1%2CtarId%3A1428961587176%2Ctas%3Aleague+of+southea" target="_blank" aria-label="LinkedIn Icon"><i class="fa fa-linkedin"><span style="display: none;">LinkedIn Icon</span></i></a></li>
+      </ul>
+    </div>
+</div>
